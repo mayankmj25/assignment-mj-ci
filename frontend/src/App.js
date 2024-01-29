e6a5e38ebb3512480e9b131ddd607ef5479a3815
mayankmj25 <250900mj@gmail.com>
2024-01-30 01:20:04 +0530

    designed sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
-
 import './App.css';
+import Revenues from './components/revenues/Revenues';
+import Sidebar from './components/sidebar/Sidebar';
+
 
 function App() {
   return (
     <div className="App">
+      <Sidebar />
+      <div className="body">
+        <div className="row">
+          <Revenues />
+        </div>
+        <div className="row"></div>
+        <div className="row"></div>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/components/revenues/Revenues.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './Revenues.module.scss'
+
+const Revenues = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default Revenues
